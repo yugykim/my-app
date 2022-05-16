83c79334594cbaee4d24ce6c7b3837fc8c5c6663
yugykim <yugyeongk92@gmail.com>
2022-05-16 15:51:58 -0600

    add hompage

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,21 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './theme';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 const container = document.getElementById('root');
-const root = createRoot(container!); 
+const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-   <QueryClientProvider client={queryClient}>
-       <ThemeProvider theme={theme}>
-         <App />
-       </ThemeProvider>
-     </QueryClientProvider>
-  </React.StrictMode>,
+    <Router basename={process.env.PUBLIC_URL}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Router>
+  </React.StrictMode >,
 );
 
